bc6e165115d2ef94c463da4f15ca3e03fd27ddd7
antocecere <antocecere77@gmail.com>
2020-04-10 09:50:33 +0200

    Passing method reference between components - anonymous function

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Anto')}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Anto')}>Switch Name</button>
         <Person 
             name={this.state.persons[0].name} 
             age={this.state.persons[0].age}
