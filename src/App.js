857f0c7cf27d6d5ee5a902c03c79da5cc2116887
antocecere <antocecere77@gmail.com>
2020-04-10 15:14:39 +0200

    Changed label

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
         <p>This is really working</p>
         <button 
             style={style}
-            onClick={this.togglePersonsHandler}>Switch Name</button>           
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>         
             { persons }  
       </div>    
     );
